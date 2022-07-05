fc348344da9dcacfb4b9b87bd572d31c839b0e91
Agustin Del Giudice <adelgiudice@stechs.com.ar>
2022-07-04 21:22:23 -0300

    probando mysql

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import pymysql
 
-engine = create_engine('mysql://root:1KW48OdR2QR7W6VELuta@containers-us-west-72.railway.app:8038/railway')
+engine = create_engine('mysql+pymysql://root:1KW48OdR2QR7W6VELuta@containers-us-west-72.railway.app:8038/railway')
 Session = sessionmaker(bind=engine)
 session = Session()
 
