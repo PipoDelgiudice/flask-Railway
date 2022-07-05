21d866dfa67a78ad71887b80a8985d17a5e538d2
Agustin Del Giudice <adelgiudice@stechs.com.ar>
2022-07-04 21:10:13 -0300

    se agrega requirements

--- db.py
+++ db.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import pymysql
 
 engine = create_engine('mysql://root:1KW48OdR2QR7W6VELuta@containers-us-west-72.railway.app:8038/railway')
 Session = sessionmaker(bind=engine)
